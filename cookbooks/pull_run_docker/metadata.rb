c5cfdee3b321bf595e3fc84280560ea81cb17091
Jenkins <hnknta@gmail.com>
2017-03-06 10:58:50 +0000

    bump version

--- cookbooks/pull_run_docker/metadata.rb
+++ cookbooks/pull_run_docker/metadata.rb
@@ -5,14 +5,12 @@ license 'all_rights'
 description 'Installs/Configures pull_run_docker'
 long_description 'Installs/Configures pull_run_docker'
 depends 'docker'
-version '0.1.19'
-
+version "1.0.51"
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
 # issues_url 'https://github.com/<insert_org_here>/pull_run_docker/issues' if respond_to?(:issues_url)
-
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
