5a72e9d2be5376b8cceb4e85d091a36eda70da63
Jenkins <hnknta@gmail.com>
2017-03-06 11:17:49 +0000

    bump version

--- cookbooks/pull_run_docker/attributes/default.rb
+++ cookbooks/pull_run_docker/attributes/default.rb
@@ -1,6 +1,6 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 default["image_name"] = "172.16.32.1:5000/task4"
-default["image_version"] = "1.0.51"
+default["image_version"] = "1.0.52"
 default["container_name"] = "tomcat"
 default["tomcat_ports"] = [8080, 8081]
